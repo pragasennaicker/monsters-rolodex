590a88c88ab3a8b963301f8ae3314cb5e06c4054
Pragasen Naicker <123pnaicker@gmail.com>
2020-07-20 21:35:33 +0200

    Updates

---
+++ precache-manifest.ef698107252be2457cc29bf64138930e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2321e16bb0be676135c59e10407181b1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ab555aac2af53e5a7642",
+    "url": "/monsters-rolodex/static/css/main.90fbf9ac.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab555aac2af53e5a7642",
+    "url": "/monsters-rolodex/static/js/main.7ed932fc.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
